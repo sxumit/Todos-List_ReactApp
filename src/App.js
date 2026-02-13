0a2b9c302e64310cae85a5671d08a8efdbee620d
Sumit Maurya <sumitmaurya746@gmail.com>
2026-02-13 22:43:42 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./MyComponents/header"
+import Todos from "./MyComponents/Todos"
+import Footer from "./MyComponents/Footer"
 
 function App() {
   return (
     <>
-      
+      <Header/>
+      <Todos/>
+      <Footer/>
     </>
   );
 }
